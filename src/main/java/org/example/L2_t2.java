8727391904b8c0b6371c2f6860cda5f7cb6eda13
warlock1305 <116345746+warlock1305@users.noreply.github.com>
2023-10-16 09:59:38 +0200

    Add files via upload

---
+++ src/main/java/org/example/L2_t2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class L2_t2{
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		String[] s = {"first", "second", "third"};
+		int ans = 0;
+		for(String i:s){
+			System.out.printf("Type %s number", i);
+			ans += reader.nextInt();
+		}
+		System.out.printf("Sum of them is %d", ans);
+	}
+}
