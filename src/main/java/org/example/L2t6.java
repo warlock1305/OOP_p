ccb240499cb02a65b778ddf14da7a90c3c2dac0c
Warlock1305 <alimarat131@gmail.com>
2023-10-16 20:53:10 +0200

    w

--- src/main/java/org/example/L2t6.java
+++ src/main/java/org/example/L2t6.java
@@ -1,2 +1,10 @@
-package org.example;public class L2t6 {
+package org.example;
+
+public class L2t6 {
+    public static void main(String[] args){
+        printText();
+    }
+    public static void printText(){
+        System.out.println("In the beginning there were the swamp, the hoe and Java");
+    }
 }
--- src/main/java/org/example/L2t9.java
+++ src/main/java/org/example/L2t9.java
@@ -1,2 +1,18 @@
-package org.example;public class L2t9 {
+package org.example;
+
+public class L2t9 {
+    public static void printNumbers(int n){
+        for(int i = 1; i <= n; i++){
+            System.out.print(i);
+        }
+        System.out.print("\n");
+    }
+    public static void drawNumbersPiramid(int n){
+        for(int i = 1; i <= n; i++){
+            printNumbers(i);
+        }
+    }
+    public static void main(String[] args){
+        drawNumbersPiramid(10);
+    }
 }
