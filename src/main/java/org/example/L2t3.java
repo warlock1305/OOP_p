3fe5694f3f3a302934ee55e0454224909fb069d3
warlock1305 <116345746+warlock1305@users.noreply.github.com>
2023-10-16 10:35:09 +0200

    Add files via upload

---
+++ src/main/java/org/example/L2t3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class L2t3{
+    public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
+        int i = reader.nextInt();
+        int sum = 0;
+        while(i != 0){
+            sum += i;
+            System.out.printf("The sum is %d ", sum);
+            i = reader.nextInt();
+        }
+        System.out.printf("In the end sum is %d ", sum);
+    }
+}
