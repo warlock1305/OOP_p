1df3bd54b0b550d8199281aa249fd4d069dbf1a5
warlock1305 <116345746+warlock1305@users.noreply.github.com>
2023-10-16 09:21:01 +0200

    Add file via terminal command

---
+++ src/main/java/org/example/L2_t1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		String password = "555ugotit";
+        System.out.println("Type the password:");
+        Scanner reader = new Scanner(System.in);
+        String s = reader.nextLine();
+        while(!s.equals(password)){
+            System.out.println("Wrong !");
+            s = reader.nextLine();
+        }
+        System.out.println("Right !");
+        System.out.println("The secret ingredient of the secret ingredient soup is ... \n Nothing!");
+    
+	}
+}
