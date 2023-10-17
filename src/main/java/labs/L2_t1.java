99c1b9334b08e7c2cbed2471c2471ad7c3389686
Warlock1305 <alimarat131@gmail.com>
2023-10-17 15:06:16 +0200

    asd

---
+++ src/main/java/labs/L2_t1.java
@@ -0,0 +1,19 @@
+package org.example;
+import java.util.Scanner;
+
+public class L2_t1
+{
+	public static void main(String[] args) {
+		String password = "555ugotit";
+        System.out.println("Type the password:");
+        Scanner reader = new Scanner(System.in);
+        String s = reader.nextLine();
+        while(!s.equals(password)){
+            System.out.println("Wrong !");
+            s = reader.nextLine();
+        }
+        System.out.println("Right !");
+        System.out.println("The secret ingredient of the secret ingredient soup is ... \n Nothing!");
+    
+	}
+}
